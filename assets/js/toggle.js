bd78453b73a3dd137da6c2c966fcda1d890922d9
mrileyowens <m.riley.owens@gmail.com>
2024-02-12 17:01:07 -0500

    Added observing programs with expanding button for more information

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,9 +1,17 @@
 function toggleText(button) {
+
   var textContainer = button.closest('.observation'); // Find the common ancestor
   var textElement = textContainer.querySelector('.info'); // Find the hidden text within the container
-  if (textElement.classList.contains('hidden')) {
-    textElement.classList.remove('hidden'); // Show the text
-  } else {
-    textElement.classList.add('hidden'); // Hide the text
-  }
+  var chevron = textContainer.querySelector('.fa');
+
+  textElement.classList.toggle('hidden');
+  chevron.classList.toggle('fa-rotate-180')
+
+  //if (textElement.classList.contains('hidden')) {
+    //textElement.classList.remove('hidden'); // Show the text
+    //chevron.classList.add('selected');
+  //} else {
+    //textElement.classList.add('hidden'); // Hide the text
+    //chevron.classList.remove('selected');
+  //}
 }
