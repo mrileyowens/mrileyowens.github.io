fdba6a6b8e7198b76f0b8af3ba71dbae24dd54d5
mrileyowens <m.riley.owens@gmail.com>
2024-02-14 16:25:10 -0500

    Added JS to 'lazy load' images and decrease site load time when first entering

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -9,3 +9,27 @@ albums.forEach(album => {
         modal.showModal();
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll("img[data-src]");
+
+    const lazyLoad = target => {
+        const io = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    const src = img.getAttribute("data-src");
+
+                    img.setAttribute("src", src);
+                    img.removeAttribute("data-src");
+
+                    observer.disconnect();
+                }
+            });
+        });
+
+        io.observe(target);
+    };
+
+    images.forEach(lazyLoad);
+});
